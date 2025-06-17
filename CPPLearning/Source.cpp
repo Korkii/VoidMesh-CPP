59beca8fd16e56acb4a93e1aa1293bf1e9ce2c10
Korkii <oryamoryam1@gmail.com>
2025-06-17 16:20:31 +0300

    fix variable name, make it print only

--- CPPLearning/Source.cpp
+++ CPPLearning/Source.cpp
@@ -9,17 +9,15 @@ const int ALLIGNMENT_MULTILPLIER = 10;
 
 /*
 Prints the multiplication table array
-
-@param table The multiplication table to print
 */
-void print_multiplication_table(int table[MULTIPLICATION_TABLE_SIZE * MULTIPLICATION_TABLE_SIZE]) {
+void print_multiplication_table() {
 	double max_identation = std::floor(std::log10((MULTIPLICATION_TABLE_SIZE * MULTIPLICATION_TABLE_SIZE)));
-	for (int i = 0; i < MULTIPLICATION_TABLE_SIZE; i++) {
-		for (int j = 0; j < MULTIPLICATION_TABLE_SIZE; j++) {
-			std::cout << table[i * MULTIPLICATION_TABLE_SIZE + j];
+	for (int i = STARTING_NUM; i < MULTIPLICATION_TABLE_SIZE + STARTING_NUM; i++) {
+		for (int j = STARTING_NUM; j < MULTIPLICATION_TABLE_SIZE + STARTING_NUM; j++) {
+			std::cout << i * j;
 
 			// Taking care of the allignmment, we go 10, 100, 1000, ... until it's at the max allignment + 1
-			for (int spaces = table[i * MULTIPLICATION_TABLE_SIZE + j]; spaces < std::pow(10, max_identation + ALLIGNMENT_OFFSET); spaces *= ALLIGNMENT_MULTILPLIER)
+			for (int spaces_counter = i*j; spaces_counter < std::pow(10, max_identation + ALLIGNMENT_OFFSET); spaces_counter *= ALLIGNMENT_MULTILPLIER)
 				std::cout << " ";
 		}
 		std::cout << std::endl;
@@ -28,13 +26,5 @@ void print_multiplication_table(int table[MULTIPLICATION_TABLE_SIZE * MULTIPLICA
 
 int main()
 {
-	int multiplication_table[MULTIPLICATION_TABLE_SIZE * MULTIPLICATION_TABLE_SIZE] = { 0 };
-
-	for (int i = STARTING_NUM; i < MULTIPLICATION_TABLE_SIZE + STARTING_NUM; i++) {
-		for (int j = STARTING_NUM; j < MULTIPLICATION_TABLE_SIZE + STARTING_NUM; j++) {
-			multiplication_table[(i - STARTING_NUM) * MULTIPLICATION_TABLE_SIZE + (j - STARTING_NUM)] = i * j;
-		}
-	}
-
-	print_multiplication_table(multiplication_table);
+	print_multiplication_table();
 }
