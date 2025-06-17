8797f99db557abd9c2eb1a5991897e26ae8f0275
Korkii <oryamoryam1@gmail.com>
2025-06-17 14:05:19 +0300

    add starting num, docstring

--- CPPLearning/Source.cpp
+++ CPPLearning/Source.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
-#define MULTIPLICATION_TABLE_SIZE 11
 
+const int MULTIPLICATION_TABLE_SIZE = 11;
+const int STARTING_NUM = 1;
 
-void print_table(int table[MULTIPLICATION_TABLE_SIZE][MULTIPLICATION_TABLE_SIZE], int width, int height) {
-	for (int i = 0; i < width; i++) {
-		for (int j = 0; j < height; j++) {
+/*
+Prints the multiplication table array
+
+@param table The multiplication table to print
+*/
+void print_multiplication_table(int table[MULTIPLICATION_TABLE_SIZE][MULTIPLICATION_TABLE_SIZE]) {
+	for (int i = 0; i < MULTIPLICATION_TABLE_SIZE; i++) {
+		for (int j = 0; j < MULTIPLICATION_TABLE_SIZE; j++) {
 			std::cout << table[i][j] << " ";
 		}
 		std::cout << std::endl;
@@ -16,12 +22,11 @@ int main()
 {
 	int multiplication_table[MULTIPLICATION_TABLE_SIZE][MULTIPLICATION_TABLE_SIZE] = { 0 };
 
-	for (int i = 0; i < MULTIPLICATION_TABLE_SIZE; i++) {
-		for (int j = 0; j < MULTIPLICATION_TABLE_SIZE; j++) {
-			multiplication_table[i][j] = i * j;
+	for (int i = STARTING_NUM; i < MULTIPLICATION_TABLE_SIZE + STARTING_NUM; i++) {
+		for (int j = STARTING_NUM; j < MULTIPLICATION_TABLE_SIZE + STARTING_NUM; j++) {
+			multiplication_table[i - STARTING_NUM][j - STARTING_NUM] = i * j;
 		}
 	}
 
-	print_table(multiplication_table, MULTIPLICATION_TABLE_SIZE, MULTIPLICATION_TABLE_SIZE);
+	print_multiplication_table(multiplication_table);
 }
-
